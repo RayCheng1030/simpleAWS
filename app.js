c6c02ded3b95eae0e984e833cfc2992dd15116c1
rainmaker96 <rainmaker96.ray@gmail.com>
2021-04-26 23:52:14 +0800

    test /ray on get methold

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/',(req,res)=>{
     res.send("<h1>Welcome to my home page</h1>");
 });
 
+app.get('/ray',(req,res)=>{
+    console.log(req.url)
+    res.send("<h1>Welcome Ray</h1>");
+});
+
 let port = process.env.port || 3000;
 app.listen(port,()=>{
     console.log("webServer is running at ",port);
