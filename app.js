3f02af4b55cab85e89b63bfe206f58c3be0712e4
rainmaker96 <rainmaker96.ray@gmail.com>
2021-04-26 11:55:22 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("<h1>Welcome to my home page</h1>");
+});
+
+let port = process.env.port || 3000;
+app.listen(port,()=>{
+    console.log("webServer is running")
+});
