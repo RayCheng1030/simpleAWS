51f5b8aba6a5859736c337d30049cc1b3f575482
rainmaker96 <rainmaker96.ray@gmail.com>
2021-04-26 12:33:01 +0800

    fix

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.get('/',(req,res)=>{
 
 let port = process.env.port || 3000;
 app.listen(port,()=>{
-    console.log("webServer is running")
+    console.log("webServer is running at ",port);
 });
